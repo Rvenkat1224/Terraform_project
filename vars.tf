3c5a2190a5145780041613a4b7055bdf1b70ca36
Venkatesan Rajendran <venkatesan.r@flyerssoft.local>
2023-02-16 18:53:15 +0530

    First commit

---
+++ vars.tf
@@ -0,0 +1,96 @@
+varaible AWS_REGION {
+    default = "us-east-1"
+}
+
+variable AMIS {
+    type = map
+    default = {
+        us-east-1 = "ami-0557a15b87f6559cf"
+        us-east-2 = "ami-0cc87e5027adcdca8"
+        ap-south-1 = "ami-0e742cca61fb65051"
+    }
+}
+
+variable PRIV_KEY_PATH {
+    default = "vprofilekey"
+}
+
+variable PUB_KEY_PATH {
+    default = "vprofilekey.pub"
+}
+
+variable USERNAME {
+    default = "ubuntu"
+}
+
+variable MYIP {
+    default = "183.83.39.124/32"
+}
+
+variable rmquser {
+    default = "rabbit"
+}
+
+variable rmqpass {
+    default = "Gr33n@pple123456"
+}
+
+variable dbuser {
+    default = "admin"
+}
+
+variable dbpass {
+    default = "admin123"
+}
+
+variable dbname {
+    default = "accounts"
+}
+
+variable instance_count {
+    default = "1"
+}
+
+variable VPC_NAME {
+    default = "vprofile-VPC"
+}
+
+variable Zone1 {
+    default = "us-east-1a"
+}
+
+variable Zone2 {
+    default = "us-east-1b"
+}
+
+variable Zone3 {
+    default = "us-east-1c"
+}
+
+variable VpcCIDR {
+    default = "172.21.0.0/16"
+}
+
+variable PubSub1CIDR {
+    default = "172.21.1.0/24"
+}
+
+variable PubSub2CIDR {
+    default = "172.21.2.0/24"
+}
+
+variable PubSub3CIDR {
+    default = "172.21.3.0/24"
+}
+
+variable PriSub1CIDR {
+    default = "172.21.4.0/24"
+}
+
+variable PriSub2CIDR {
+    default = "172.21.5.0/24"
+}
+
+variable PriSub3CIDR {
+    default = "172.21.6.0/24"
+}
